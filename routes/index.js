7b964ba2e0a925542aa69f9fdf284b76b1eeec55
John Britton <johnbritton@Enterprise-5.local>
2022-10-31 11:55:58 -0600

    Configure Nav

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/', function(req, res, next) {
+  res.redirect('/users');
+});
+
+router.get('/auth/google', passport.authenticate(
+  // which passport stategy to use
+  'google',
+  {
+    scope: ['profile', 'email'],
+    //optional
+    prompt: 'select_account'
+  }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    //this is if the user accepts the auth or denies it.
+    successRedirect: '/users',
+    failureRedirect: '/users'
+  }
+));
+
+router.get('/logout', function(req, res) {
+  req.logOut(function() {
+    res.redirect('/users')
+  });
+
+});
 module.exports = router;
+
+
